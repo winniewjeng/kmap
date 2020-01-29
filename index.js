e8ebf915cb0321a700173cf9584d2c7862873147
Winnie <winniewjeng@gmail.com>
2020-01-29 13:40:46 -0800

    update fxns

--- index.js
+++ index.js
@@ -1,25 +1,89 @@
 `use strict`;
 
 console.log("hello");
-let arr = [0,0,0,0,0,0,0,0];
-
+let arr3 = [0,0,0,0,0,0,0,0];
+var newGroup = [];
+var submit = false;
 function changeState(address) {
-    arr[convertToBinary(address)]++;
-    if(arr[convertToBinary(address)]%3==1) {
+    arr3[convertToBinary(address)]++;
+    if(arr3[convertToBinary(address)]%3==1) { // 1
         document.getElementById(`grid `+ address).style.backgroundColor = 'green';
-    } else if (arr[convertToBinary(address)]%3==2) {
+        toGroups(address);
+        // console.log(address);
+    } else if (arr3[convertToBinary(address)]%3==2) { // don't care
         document.getElementById(`grid `+ address).style.backgroundColor = 'gray';
-    } else {
+    } else { // 0
         document.getElementById(`grid `+ address).style.backgroundColor = 'orangered';
+        removeFromGroup(address);
     }
-    // printArr();
+    // printArr3();
+}
+
+function removeFromGroup(address) {
+    for (var i=0; i < newGroup.length; i++) {
+        if (newGroup[i]===address) {
+            newGroup.splice(i, 1);
+        }
+    }
+}
+
+function calculate() {
+
+    if (submit == false) {
+
+    }
+    console.log(newGroup);
+    submit = true;
 }
 
 function convertToBinary(address) {
     // console.log(address[0]*4+address[1]*2+address[2]*1);
-    return address[0]*4+address[1]*2+address[2]*1
+    return address[0]*4+address[1]*2+address[2]*1;
 }
 
-function printArr() {
-    console.log(arr)
+function printArr3() {
+    console.log(arr3);
 }
+
+function toGroups(address) {
+    newGroup.push(address);
+    quickSortAddress(newGroup, 0, newGroup.length-1);
+}
+
+function quickSortAddress(arr, left, right) {
+    var pivot, splitIndex;
+
+    if (left < right) {
+        pivot = right;
+        splitIndex = split(arr, pivot, left, right);
+
+        // sort left and right
+        quickSortAddress(arr, left, splitIndex-1);
+        quickSortAddress(arr, splitIndex+1, right);
+    }
+    
+    // console.log(newGroup);
+
+}
+
+function split(arr, pivot, left, right) {
+    var pivotValue = arr[pivot];
+    var splitIndex = left;
+
+    for(var i = left; i < right; i++) {
+        if (arr[i]<pivotValue) {
+            swapAddresses(arr, i, splitIndex);
+            splitIndex++;
+        }
+    }
+    swapAddresses(arr, right, splitIndex);
+    return splitIndex;
+}
+
+function swapAddresses(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
